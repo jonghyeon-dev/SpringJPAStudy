228a7e658011a102468eb1957fb9966c019d1fcd
jonghyeon-dev <58764963+jonghyeon-dev@users.noreply.github.com>
2023-12-04 23:36:57 +0900

    slf4j 로거 적용 (#1)
    확인

--- src/main/java/com/jpatest/demo/config/Interceptor.java
+++ src/main/java/com/jpatest/demo/config/Interceptor.java
@@ -1,6 +1,7 @@
 package com.jpatest.demo.config;
 
-import org.apache.taglibs.standard.lang.jstl.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
@@ -15,10 +16,12 @@ import jakarta.servlet.http.HttpSession;
 
 @Component
 public class Interceptor implements HandlerInterceptor{
+    private Logger logger = LoggerFactory.getLogger(Interceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+        logger.info("Interceptor preHandle");
         HttpSession session = request.getSession();
         UserVO loginVO = (UserVO) session.getAttribute("loginUser");
         // 예외 URI 적용
@@ -41,13 +44,14 @@ public class Interceptor implements HandlerInterceptor{
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
+                logger.info("Interceptor postHandle");
         
     }
  
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
             throws Exception {
-        
+        logger.info("Interceptor afterHandle");
     }
 
     
